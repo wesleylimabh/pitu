c51737e9052cba6aa98762ded9d3ff404a697569
Wesley Lima <wesleylima@gmx.com>
2020-11-24 12:17:42 -0300

    add cors dependency

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
+import cors from 'cors'
 import linksRouter from './routes/linksRoutes'
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 app.use(linksRouter);
 
 export default app;
