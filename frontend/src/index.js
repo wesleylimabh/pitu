a4c6e5fe9cfec75ffa578a0e8548cdcf3455bc47
Wesley Lima <wesleylima@gmx.com>
2020-11-25 13:17:11 -0300

    include StatsPage

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -4,9 +4,9 @@ import App from './App';
 import './App.scss'
 
 import { library } from '@fortawesome/fontawesome-svg-core'
-import { faCheckSquare } from '@fortawesome/free-solid-svg-icons'
+import { faExclamationTriangle } from '@fortawesome/free-solid-svg-icons'
 
-library.add(faCheckSquare)
+library.add(faExclamationTriangle)
 
 ReactDOM.render(
   <React.StrictMode>
--- frontend/src/pages/StatsPage/index.js
+++ frontend/src/pages/StatsPage/index.js
@@ -1,13 +1,73 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { parseISO, formatRelative } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
 
+import Header from '../../components/Header';
+import ShortenerService from '../../services/shortenerService'
+import { StatsBox, StatsBoxTitle, StatsContainer, StatsRow } from './styles'
 class StatsPage extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      isLoading: false,
+      shortenedURL: {},
+      errorMessage: '',
+    }
+  }
+
+  async componentDidMount() {
+    const { code } = this.props.match.params;
+    try {
+      const service = new ShortenerService();
+      const shortenedURL = await service.getStats(code);
+
+      const parsedDate = parseISO(shortenedURL.updatedAt);
+      const currentDate = new Date();
+
+      const relativeDate = formatRelative(parsedDate, currentDate, {
+        locale: ptBR,
+      });
+
+      shortenedURL.relativeDate = relativeDate;
+
+      this.setState({ isLoading: false, shortenedURL })
+    } catch (error) {
+      this.setState({ isLoading: false, errorMessage: 'Ops, a url solicitada não existe.'})
+    }
   }
 
   render() {
+    const {errorMessage, shortenedURL} = this.state;
     return (
-      <p>Stats</p>
+      <Container>
+        <Header>Estatísticas</Header>
+        {errorMessage ? (
+          <StatsContainer className="text-center">
+            <FontAwesomeIcon size="3x" color="#f8d7da" icon="exclamation-triangle" />
+            <p className="m-3">{errorMessage}</p>
+            <a href="/" className="btn btn-primary">Encurtar nova URL</a>
+          </StatsContainer>
+        ) : (
+          <StatsContainer className="text-center">
+            <p><b>https://pitu.tk/{shortenedURL.code}</b></p>
+            <p>Redireciona para:<br/>{shortenedURL.url}</p>
+            <StatsRow>
+              <StatsBox>
+                <b>{shortenedURL.hits}</b>
+                <StatsBoxTitle>Visitas</StatsBoxTitle>
+              </StatsBox>
+              <StatsBox>
+                <b>{shortenedURL.relativeDate}</b>
+                <StatsBoxTitle>Última visita</StatsBoxTitle>
+              </StatsBox>
+            </StatsRow>
+            <a className="btn btn-primary" href="/">Encurtar nova URL</a>
+          </StatsContainer>
+        )}
+      </Container>
     )
   }
 }
