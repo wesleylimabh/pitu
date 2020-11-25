0d38c6b0e9e129d26bc75cb6e714a1b45711b52d
Wesley Lima <wesleylima@gmx.com>
2020-11-25 19:46:52 -0300

    add PageNotFound

--- frontend/src/pages/NotFoundPage/index.js
+++ frontend/src/pages/NotFoundPage/index.js
@@ -1,14 +1,29 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+
+import Header from '../../components/Header'
+import { PageNotFoundContainer } from './styles'
+
 
 class NotFoundPage extends React.Component {
   constructor(props) {
     super(props);
+
+    this.errorMessage = 'Página não encontrada'
   }
 
   render() {
     return (
-      <p>Page Not Found</p>
-    )
+      <>
+        <Header>Seu novo encurtador de URL</Header>
+  
+        <PageNotFoundContainer className="text-center">
+          <FontAwesomeIcon size="3x" color="#f8d7da" icon="exclamation-triangle" />
+          <p className="m-3"><b>{this.errorMessage} <br/> :(</b></p>
+          <a href="/" className="btn btn-primary">Encurtar nova URL</a>
+        </PageNotFoundContainer>
+      </>
+      )
   }
 }
 
---
+++ frontend/src/pages/NotFoundPage/styles.js
@@ -0,0 +1,5 @@
+import styled from 'styled-components';
+
+export const PageNotFoundContainer = styled.div`
+  display: block;
+`;
