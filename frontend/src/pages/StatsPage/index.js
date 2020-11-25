bfbd0416794c040dba2bba557a453a4365af3ff5
Wesley Lima <wesleylima@gmx.com>
2020-11-25 18:53:23 -0300

    refactor StatsPage code

--- frontend/src/pages/StatsPage/index.js
+++ frontend/src/pages/StatsPage/index.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Spinner } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { parseISO, formatRelative } from 'date-fns'
 import ptBR from 'date-fns/locale/pt-BR'
@@ -22,16 +22,11 @@ class StatsPage extends React.Component {
     const { code } = this.props.match.params;
     try {
       const service = new ShortenerService();
+      this.setState({ isLoading: true })
+
       const shortenedURL = await service.getStats(code);
 
-      const parsedDate = parseISO(shortenedURL.updatedAt);
-      const currentDate = new Date();
-
-      const relativeDate = formatRelative(parsedDate, currentDate, {
-        locale: ptBR,
-      });
-
-      shortenedURL.relativeDate = relativeDate;
+      shortenedURL.relativeDate = this._getRelativeDate(shortenedURL.updatedAt);
 
       this.setState({ isLoading: false, shortenedURL })
     } catch (error) {
@@ -39,34 +34,57 @@ class StatsPage extends React.Component {
     }
   }
 
+  _getRelativeDate(date) {
+    const parsedDate = parseISO(date);
+    const currentDate = new Date();
+
+    const relativeDate = formatRelative(parsedDate, currentDate, {
+      locale: ptBR,
+    });
+    return relativeDate;
+  }
+
+  _componentError() {
+    const {errorMessage} = this.state;
+    return (
+      <>
+      <FontAwesomeIcon size="3x" color="#f8d7da" icon="exclamation-triangle" />
+      <p className="m-3">{errorMessage}</p>
+      </>
+    )
+  }
+
+  _componentSuccess() {
+    const {code, url, hits, relativeDate } = this.state.shortenedURL;
+
+    return (
+      <>
+      <p><b>https://pitu.tk/{code}</b></p>
+      <p>Redireciona para:<br/>{url}</p>
+      <StatsRow>
+        <StatsBox>
+          <b>{hits}</b>
+          <StatsBoxTitle>Visitas</StatsBoxTitle>
+        </StatsBox>
+        <StatsBox>
+          <b>{relativeDate}</b>
+          <StatsBoxTitle>Última visita</StatsBoxTitle>
+        </StatsBox>
+      </StatsRow>
+      </>
+    )
+  }
+
   render() {
-    const {errorMessage, shortenedURL} = this.state;
+    const {errorMessage, isLoading} = this.state;
     return (
       <Container>
         <Header>Estatísticas</Header>
-        {errorMessage ? (
           <StatsContainer className="text-center">
-            <FontAwesomeIcon size="3x" color="#f8d7da" icon="exclamation-triangle" />
-            <p className="m-3">{errorMessage}</p>
-            <a href="/" className="btn btn-primary">Encurtar nova URL</a>
+            { isLoading ? <Spinner animation="border" /> :
+                errorMessage ? this._componentError() : this._componentSuccess() }
+            { !isLoading ? <a href="/" className="btn btn-primary">Encurtar nova URL</a> : ""}
           </StatsContainer>
-        ) : (
-          <StatsContainer className="text-center">
-            <p><b>https://pitu.tk/{shortenedURL.code}</b></p>
-            <p>Redireciona para:<br/>{shortenedURL.url}</p>
-            <StatsRow>
-              <StatsBox>
-                <b>{shortenedURL.hits}</b>
-                <StatsBoxTitle>Visitas</StatsBoxTitle>
-              </StatsBox>
-              <StatsBox>
-                <b>{shortenedURL.relativeDate}</b>
-                <StatsBoxTitle>Última visita</StatsBoxTitle>
-              </StatsBox>
-            </StatsRow>
-            <a className="btn btn-primary" href="/">Encurtar nova URL</a>
-          </StatsContainer>
-        )}
       </Container>
     )
   }
