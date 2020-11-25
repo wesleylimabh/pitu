92a93edfeacf0945c77a61050cb68c6520724eb5
Wesley Lima <wesleylima@gmx.com>
2020-11-25 14:53:57 -0300

    included RedirectPage

--- frontend/src/pages/RedirectPage/index.js
+++ frontend/src/pages/RedirectPage/index.js
@@ -1,13 +1,54 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
+import Header from '../../components/Header';
+import ShortenerService from '../../services/shortenerService';
+import { RedirectContainer } from './styles'
 class RedirectPage extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      isLoading: false,
+      url: '',
+      errorMessage: '',
+    }
+  }
+
+  async componentDidMount(){
+    const { code } = this.props.match.params;
+
+    try {
+      const service = new ShortenerService();
+      const { url } = await service.getLink(code)
+      
+      window.location = url;
+    } catch (error) {
+      this.setState({isLoading: false, errorMessage: 'Ops, a url solicitada não existe.'})
+    }
   }
 
   render() {
+    const {errorMessage} = this.state;
     return (
-      <p>Redirect</p>
+      (
+        <Container>
+          {errorMessage ? (
+            <>
+              <Header>Seu novo encurtador de urls.</Header>
+
+              <RedirectContainer className="text-center">
+                <FontAwesomeIcon size="3x" color="#f8d7da" icon="exclamation-triangle" />
+                <p className="m-3">{errorMessage}</p>
+                <a href="/" className="btn btn-primary">Encurtar nova URL</a>
+              </RedirectContainer>
+            </>
+          ) : (
+            <p className="text-center">Redirecionando...</p>
+          )}
+        </Container>
+      )
     )
   }
 }
